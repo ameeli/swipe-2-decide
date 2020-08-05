8bbff41008504d9580d93cc26ab7f061019f7fd9
Amee Li <ameejli@gmail.com>
2020-08-05 10:55:31 -0700

    Access secret key

--- swipe_2_decide/settings.py
+++ swipe_2_decide/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-2#*c4y&mlb!hue*hc^xbvf*(1qiegb=gi(bbc-onp895_*7)_'
+with open('secret_key.txt') as f:
+    SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
