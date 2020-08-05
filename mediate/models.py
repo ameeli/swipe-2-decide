97a01449da6e19fc45751cb1b82ea23cb3761e5f
Amee Li <ameejli@gmail.com>
2020-08-05 11:44:00 -0700

    Add user model and create initial migration

--- mediate/models.py
+++ mediate/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    password = models.CharField(max_length=50)  # Update to use forms.
+    created_at = models.DateTimeField(auto_now_add=True)
