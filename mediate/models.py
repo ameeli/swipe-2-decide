f1a10682c48973c0c9c79f56ffe69abb3edd2c17
Amee Li <ameejli@gmail.com>
2020-08-14 16:32:44 -0700

    Add RestaurantMatch and foreign key to User

--- mediate/models.py
+++ mediate/models.py
@@ -1,8 +1,14 @@
+from django.contrib.postgres.fields import ArrayField
 from django.db import models
+
+
+class RestaurantMatch(models.Model):
+    time_created = models.DateTimeField()
+    restaurants = ArrayField(ArrayField(models.TextField()))
 
 
 class User(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField()
-    password = models.CharField(max_length=50)  # Update to use forms.
-    created_at = models.DateTimeField(auto_now_add=True)
+    restaurant_match_id = models.ForeignKey(
+        RestaurantMatch, default=None, on_delete=models.CASCADE)
